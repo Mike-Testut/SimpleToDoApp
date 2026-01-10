99f98ae1063cbe875d7511aa5594b4c0f2f34f5d
Mike-Testut <michael.testut@gmail.com>
2026-01-10 10:53:36 -0800

    add remove function and fix numbering system

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from unittest import case
 
-user_prompt = "Type 'add', 'show', or 'exit': "
+user_prompt = "Type 'add', 'show', 'remove' or 'exit': "
 
 to_do_list = []
-
 while True:
-    user_input = input(user_prompt)
+    user_input = input(user_prompt).strip()
     match user_input:
         case "add":
             to_do = input("Enter to do item: ")
             to_do_list.append(to_do)
         case "show":
-            print(to_do_list)
+            for index,item in enumerate(to_do_list):
+                print(f"{index+1}. {item}")
+        case "remove":
+            for index,item in enumerate(to_do_list):
+                print(f"{index+1}. {item}")
+            item_to_delete = input("Which number do you want to remove?: ")
+            to_do_list.pop(int(item_to_delete)-1)
         case "exit":
             print("Goodbye")
             break
