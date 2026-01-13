a736ed99116a36ffd39934198f2600a61c20c4bf
Mike-Testut <michael.testut@gmail.com>
2026-01-12 23:28:50 -0800

    update syntax and update remove case

--- main.py
+++ main.py
@@ -8,20 +8,22 @@ while True:
     match user_input:
         case "add":
             to_do = input("Enter to do item: ") +"\n"
-            file = open("to_do_items.txt", "a")
-            to_dos.append(to_do)
-            file.writelines(to_do)
-            file.close()
+            with open("to_do_items.txt", "a") as file:
+                file.writelines(to_do)
         case "show":
-            file = open("to_do_items.txt", "r")
-            to_do_list = file.readlines()
+            with open("to_do_items.txt", "r") as file:
+                to_do_list = file.readlines()
             for index,item in enumerate(to_do_list):
-                print(f"{index+1}. {item}")
+                print(f"{index+1}. {item.strip("\n")}")
         case "remove":
-            for index,item in enumerate(to_dos):
-                print(f"{index+1}. {item}")
+            with open("to_do_items.txt", "r") as file:
+                to_do_list = file.readlines()
+            for index,item in enumerate(to_do_list):
+                print(f"{index+1}. {item.strip("\n")}")
             item_to_delete = input("Which number do you want to remove?: ")
-            to_dos.pop(int(item_to_delete)-1)
+            to_do_list.pop(int(item_to_delete)-1)
+            with open("to_do_items.txt", "w") as file:
+                file.writelines(to_do_list)
         case "exit":
             print("Goodbye")
             break
