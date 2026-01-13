4ae2f4910d1ad7e77e0d5a853b71342ab2dd12c1
Mike-Testut <michael.testut@gmail.com>
2026-01-13 14:25:55 -0800

    update syntax from match case to if/else

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 
-user_prompt = "Type 'add', 'show', 'remove' or 'exit': "
+user_prompt = "Type 'add'+ your to-do item, or type 'show', 'remove' or 'exit': "
 
 while True:
     user_input = input(user_prompt).strip()
-    match user_input:
-        case "add":
-            to_do = input("Enter to do item: ") +"\n"
-            with open("to_do_items.txt", "a") as file:
-                file.writelines(to_do)
-        case "show":
-            with open("to_do_items.txt", "r") as file:
-                to_do_list = file.readlines()
-                if not to_do_list:
-                    print("Nothing to do, please add an item")
-                else:
-                    for index,item in enumerate(to_do_list):
-                        print(f"{index+1}. {item.strip("\n")}")
-        case "remove":
-            with open("to_do_items.txt", "r") as file:
-                to_do_list = file.readlines()
-            for index,item in enumerate(to_do_list):
-                print(f"{index+1}. {item.strip("\n")}")
-            item_to_delete = input("Which number do you want to remove?: ")
-            if 0 < int(item_to_delete) <= len(to_do_list):
-                to_do_list.pop(int(item_to_delete)-1)
-                with open("to_do_items.txt", "w") as file:
-                    file.writelines(to_do_list)
-                print("Item removed successfully")
+    if "add" in user_input:
+        # to_do = input("Enter to do item: ") +"\n"
+        with open("to_do_items.txt", "a") as file:
+            file.writelines(user_input[4:]+"\n")
+    elif user_input == "show":
+        with open("to_do_items.txt", "r") as file:
+            to_do_list = file.readlines()
+            if not to_do_list:
+                print("Nothing to do, please add an item")
             else:
-                print("Item not in list")
-        case "exit":
-            print("Goodbye")
-            break
+                for index,item in enumerate(to_do_list):
+                    print(f"{index+1}. {item.strip("\n")}")
+    elif user_input == "remove":
+        with open("to_do_items.txt", "r") as file:
+            to_do_list = file.readlines()
+        for index,item in enumerate(to_do_list):
+            print(f"{index+1}. {item.strip("\n")}")
+        item_to_delete = input("Which number do you want to remove?: ")
+        if 0 < int(item_to_delete) <= len(to_do_list):
+            to_do_list.pop(int(item_to_delete)-1)
+            with open("to_do_items.txt", "w") as file:
+                file.writelines(to_do_list)
+            print("Item removed successfully")
+        else:
+            print("Item not in list")
+    elif user_input == "exit":
+        print("Goodbye")
+        break
+    else:
+        print("Invalid input")
