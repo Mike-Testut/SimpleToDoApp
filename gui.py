9f0efab067e5abb31b824d3251a239a5df9783cb
Mike-Testut <michael.testut@gmail.com>
2026-01-21 18:46:37 -0800

    add remove function to gui

--- gui.py
+++ gui.py
@@ -7,17 +7,22 @@ input_box = gui.InputText(key="input")
 list_box = gui.Listbox(f.retrieve_items())
 add_button = gui.Button("Add")
 list_box = gui.Listbox(f.retrieve_items(),key="todo_items",enable_events=True,size=(45,10))
-
+remove_button = gui.Button("Remove")
 window = gui.Window("To Do App",
-                    layout=[[label], [input_box, add_button],[list_box]],
+                    layout=[[label], [input_box, add_button],[list_box, remove_button]],
                     font=("Helvetica", 16))
 
 while True:
     event, values = window.read()
-    print(event, values)
+
     match event:
         case "Add":
             f.write_items("a",values['input'] + "\n")
+            window["todo_items"].update(f.retrieve_items())
+        case "Remove":
+            to_do_list = f.retrieve_items()
+            to_do_list.remove(values['todo_items'][0])
+            f.write_items("w", to_do_list)
             window["todo_items"].update(f.retrieve_items())
         case gui.WIN_CLOSED:
             break
