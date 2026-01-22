3df0a177ecbdf7ab4e46232620787d41a96f8b3d
Mike-Testut <michael.testut@gmail.com>
2026-01-21 20:06:07 -0800

    bug fixes

--- gui.py
+++ gui.py
@@ -1,13 +1,13 @@
+from contextlib import nullcontext
+
 import functions as f
 import FreeSimpleGUI as gui
 
-
 label = gui.Text("Add your to do item")
 input_box = gui.InputText(key="input")
-list_box = gui.Listbox(f.retrieve_items())
 add_button = gui.Button("Add")
 list_box = gui.Listbox(f.retrieve_items(),key="todo_items",enable_events=True,size=(45,10))
-remove_button = gui.Button("Remove")
+remove_button = gui.Button("Remove", disabled = True)
 window = gui.Window("To Do App",
                     layout=[[label], [input_box, add_button],[list_box, remove_button]],
                     font=("Helvetica", 16))
@@ -17,13 +17,22 @@ while True:
 
     match event:
         case "Add":
-            f.write_items("a",values['input'] + "\n")
-            window["todo_items"].update(f.retrieve_items())
+            if values['input'] != '':
+                f.write_items("a",values['input'] + "\n")
+                window["todo_items"].update(f.retrieve_items())
+                window["input"].update("")
+
         case "Remove":
             to_do_list = f.retrieve_items()
             to_do_list.remove(values['todo_items'][0])
             f.write_items("w", to_do_list)
             window["todo_items"].update(f.retrieve_items())
+            window["Remove"].update(disabled=True)
+
+        case "todo_items":
+            if values["todo_items"][0] is not None:
+                window["Remove"].update(disabled=False)
+
         case gui.WIN_CLOSED:
             break
 
